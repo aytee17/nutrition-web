1e4783145dfad7598a12a12ff86a04a2a9c26e26
Andrew Tang <andrew.tang@outlook.com>
2018-07-21 22:26:54 +1000

    Add ingredient search to AddMeal

--- src/components/AddMeal.js
+++ src/components/AddMeal.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import style from "./AddMeal.scss";
 import { Button, InputList, Input, Label, SmallLabel } from "./UI/Inputs";
 import { AddIcon, CloseIcon } from "./Icons/Icons";
 import { Pane, Horizontal } from "./Templates/Templates";
+import SearchList from "./SearchList";
 
 export default class AddMeal extends Component {
 	constructor(props) {
@@ -43,7 +44,6 @@ export default class AddMeal extends Component {
 			<Horizontal style={{ justifyContent: "flex-start" }}>
 				<div>
 					<Button
-						style={{ marginTop: "0.3rem" }}
 						pressed={active}
 						disabled={active}
 						onClick={this.toggleActive}
@@ -52,49 +52,67 @@ export default class AddMeal extends Component {
 					</Button>
 				</div>
 				{active && (
-					<Pane
-						style={{
-							padding: "0.5rem",
-							marginLeft: "0.5rem",
-							position: "relative"
-						}}
-						fade
-					>
-						<CloseIcon
-							className={style["close"]}
-							onClick={this.toggleActive}
-						/>
-
-						<div className={style["top"]}>
-							<Input
-								name="title"
-								value={title}
-								onChange={this.updateValue}
-								onKeyDown={this.completeValue}
-								autoFocus
-							>
-								<Label>Meal Name</Label>
-							</Input>
-							<Input
-								name="serves"
-								style={{ width: "4rem", paddingTop: "19px" }}
-								value={serves}
-								onFocus={event => event.target.select()}
-								onChange={this.updateServes}
-								onKeyDown={this.completeValue}
-								onBlur={this.onServingsBlur}
-								type="number"
-								min={1}
-								max={99}
-								number
-								noUnits
-							>
-								<Label>Serves</Label>
-							</Input>
-						</div>
-					</Pane>
+					<div>
+						<Pane
+							style={{
+								padding: "0.7rem",
+								marginLeft: "0.5rem",
+								position: "relative"
+							}}
+							fade
+						>
+							<CloseIcon
+								className={style["close"]}
+								onClick={this.toggleActive}
+							/>
+							<div className={style["top"]}>
+								<Input
+									name="title"
+									value={title}
+									onChange={this.updateValue}
+									onKeyDown={this.completeValue}
+									autoFocus
+								>
+									<Label>Meal Name</Label>
+								</Input>
+								<Input
+									name="serves"
+									style={{
+										width: "4rem",
+										paddingTop: "19px"
+									}}
+									value={serves}
+									onFocus={event => event.target.select()}
+									onChange={this.updateServes}
+									onKeyDown={this.completeValue}
+									onBlur={this.onServingsBlur}
+									type="number"
+									min={1}
+									max={99}
+									number
+									noUnits
+								>
+									<Label>Serves</Label>
+								</Input>
+							</div>
+							<div className={style["section"]}>
+								<strong>Ingredients</strong>
+								<div className={style["list"]} />
+								<SearchList/>
+							</div>
+						</Pane>
+					</div>
 				)}
 			</Horizontal>
 		);
 	}
 }
+
+/**
+ * 								<input
+									className={style["search"]}
+									type="text"
+									placeholder="Search for an ingredient"
+								/>
+
+ */
